31f80d93423ac63a2d49cc78afaa9b3affe55e1c
Steve Smith <tarkasteve@gmail.com>
2025-10-01 10:51:41 +1000

    Break certificate support into module.

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,26 @@
 
+mod certificates;
+
 use std::str::FromStr;
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use async_trait::async_trait;
 use pingora::{
     http::RequestHeader,
-    listeners::{tls::TlsSettings, TlsAccept},
+    listeners::tls::TlsSettings,
     prelude::HttpPeer,
-    protocols::tls::TlsRef,
     proxy::{http_proxy_service, ProxyHttp, Session},
     server::Server,
-    tls::{pkey::{PKey, Private}, ssl::NameType, x509::X509}
 };
 use tracing::info;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
+use crate::certificates::Callbacks;
+
+
 const TEST_HOSTS: [&str; 2] = ["dvalinn.haltcondition.net", "adguard.haltcondition.net"];
-const TEST_DIR: &str = "tests/data/certs/acme";
 
-
-struct HostCertificate {
-    key: PKey<Private>,
-    certs: Vec<X509>,
-}
-
-struct Callbacks {
-    certmap: papaya::HashMap<String, HostCertificate>,
-}
-
-fn load_cert_files(host: &str) -> Result<(Vec<u8>, Vec<u8>)> {
-    let keyfile = std::fs::read(format!("{TEST_DIR}/{host}.key"))?;
-    let certfile = std::fs::read(format!("{TEST_DIR}/{host}.crt"))?;
-
-    Ok((keyfile, certfile))
-}
-
-fn from_files(keyfile: Vec<u8>, certfile: Vec<u8>) -> Result<HostCertificate> {
-    let key = PKey::private_key_from_pem(&keyfile)?;
-    let certs = X509::stack_from_pem(&certfile)?;
-    if certs.is_empty() {
-        bail!("No certificates found in TLS .crt file");
-    }
-
-    let hostcert = HostCertificate { key, certs };
-
-    Ok(hostcert)
-}
-
-
-impl Callbacks {
-    fn new(hosts: Vec<&str>) -> Result<Self> {
-        info!("Loading host certificates");
-
-        let certmap = hosts.iter()
-            .map(|host| {
-                let (key, cert) = load_cert_files(host)?;
-                let cert = from_files(key, cert)?;
-                Ok((host.to_string(), cert))
-            })
-            .collect::<Result<_>>()?;
-
-        info!("Loaded certificates");
-
-        Ok(Callbacks { certmap })
-    }
-
-}
-
-#[async_trait]
-impl TlsAccept for Callbacks {
-
-    // NOTE:This is all boringssl specific as pingora doesn't
-    // currently support dynamic certs with rustls.
-    async fn certificate_callback(&self, ssl: &mut TlsRef) -> () {
-        let host = ssl.servername(NameType::HOST_NAME)
-            .expect("No servername in TLS handshake");
-
-        info!("TLS Host is {host}; loading certs");
-
-        //        let cert = load_cert(host).await.unwrap();
-        let amap = self.certmap.pin_owned();
-        let cert = amap.get(&host.to_string())
-            .expect("Certificate for host not found");
-
-        ssl.set_private_key(&cert.key)
-            .expect("Failed to set private key");
-        ssl.set_certificate(&cert.certs[0])
-            .expect("Failed to set certificate");
-
-        if cert.certs.len() > 1 {
-            for c in cert.certs[1..].iter() {
-                ssl.add_chain_cert(&c)
-                    .expect("Failed to add chain certificate");
-            }
-        }
-    }
-
-}
 
 fn init_logging(level: &Option<String>) -> anyhow::Result<()> {
     let lf = level.clone()
