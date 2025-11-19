8f41c2fe329f15fbd0d13a0bc5fda816eaad7d29
Steve Smith <tarkasteve@gmail.com>
2025-11-19 13:22:08 +1100

    Messy interim checkin.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,39 +1,16 @@
 
 mod util;
 
-use std::process::Child;
 
 use anyhow::Result;
-use test_context::{TestContext, test_context};
+use test_context::test_context;
 use test_log::test;
-use tracing_log::log::info;
 
-use crate::util::{run_proxy, stop_child, wait_port};
-
-struct IntegrationTest {
-    proxy: Child,
-    proxy_port: u16,
-}
-
-impl TestContext for IntegrationTest {
-    fn setup() -> Self {
-        info!("Starting proxy");
-        let proxy = run_proxy().expect("Failed to start proxy");
-        let proxy_port = wait_port(&proxy).expect("Failed to find proxy port");
-        Self {
-            proxy,
-            proxy_port,
-        }
-    }
-
-    fn teardown(self) {
-        stop_child(&self.proxy).unwrap();
-    }
-}
+use crate::util::IntegrationTest;
 
 #[test_context(IntegrationTest)]
 #[test]
 fn test_fetch_port(ctx: &IntegrationTest) -> Result<()> {
-    println!("Port is {}", ctx.proxy_port);
+    //    println!("Port is {}", ctx.proxy_port);
     Ok(())
 }
--- tests/util.rs
+++ tests/util.rs
@@ -1,69 +1,142 @@
-use std::{net::SocketAddr, process::{Child, Command}};
+use std::{net::{IpAddr, SocketAddr}, process::{Child, Command}, sync::{Arc, LazyLock, OnceLock}};
 use std::thread;
 use std::time::Duration;
 
 use anyhow::{Result, bail};
+use axum::extract::State;
+use axum::{routing, Json, Router};
+use ctor::dtor;
 use nix::{sys::signal::{Signal, kill}, unistd::Pid};
 use procfs::{
     process::{FDTarget, Process},
     net::{tcp as tcp_table, tcp6 as tcp6_table}
 };
+use test_context::TestContext;
+use tokio::net::TcpListener;
+use tokio::select;
+use tokio::sync::{
+    mpsc::{self, Receiver, Sender},
+    Notify,
+};
+use tokio::task::JoinHandle;
+use tokio::{fs::read_to_string, time::sleep};
 use tracing_log::log::info;
 
+static PROXY: OnceLock<Child> = OnceLock::new();
 
-pub fn run_proxy() -> Result<Child> {
-    let exe = env!("CARGO_BIN_EXE_proxeny");
+const PROXY_PORT: u16 = 8080;
+const PROXY_TLS_PORT: u16 = 8443;
 
-    let child = Command::new(exe)
-        .arg("-vv")
-        .arg("-c").arg("proxeny.corn")
-        .spawn()?;
-    Ok(child)
-}
+pub fn run_proxy() -> Result<&'static Child> {
+    let child = PROXY.get_or_init(|| {
+        info!("Starting proxy");
+        let exe = env!("CARGO_BIN_EXE_proxeny");
 
-pub fn stop_child(child: &Child) -> Result<()> {
-    let pid = Pid::from_raw(child.id().try_into()?);
-    kill(pid, Signal::SIGINT)?;
-    Ok(())
-}
+        let child = Command::new(exe)
+            .arg("-vv")
+            .arg("-c").arg("proxeny.corn")
+            .spawn()
+            .expect("Failed to start proxy");
+        child
+    });
 
-pub fn get_proc_port(child: &Child) -> Result<Option<u16>> {
-    let proc = Process::new(child.id() as i32)?;
-    let inodes = proc.fd()?
-        .filter_map(|fd| match fd.unwrap().target {
-            FDTarget::Socket(inode) => Some(inode),
-            _ => None
-        })
-        .collect::<Vec<u64>>();
-
-    for inode in inodes {
-        let addrs = tcp_table()?.into_iter()
-            .chain(tcp6_table()?)
-            .filter_map(|t| if t.inode == inode {
-                Some(t.local_address)
-            } else {
-                None
-            })
-            .collect::<Vec<SocketAddr>>();
-
-        if addrs.len() > 0 {
-            info!("Found addresses {addrs:#?}");
-            return Ok(Some(addrs[0].port()))
-        };
+    for _ in 0..20 { // 2 second timeout
+        let ready = reqwest::blocking::get(format!("http://localhost:{PROXY_PORT}/status"))
+            .is_ok_and(|r| r.status().is_success());
+        if ready {
+            return Ok(child);
+        }
+        thread::sleep(Duration::from_millis(100));
     }
-
-    Ok(None)
+    bail!("Failed to start proxy server")
 }
 
-pub fn wait_port(child: &Child) -> Result<u16> {
-    const WAIT_MS: u64 = 10;
-    const WAIT_SECS: u64 = 10;
-    const WAIT_TIMES: u64 = WAIT_SECS * 1000 / WAIT_MS;
-    for _ in 0..WAIT_TIMES {
-        thread::sleep(Duration::from_millis(WAIT_MS));
-        if let Some(port) = get_proc_port(child)? {
-            return Ok(port)
+#[dtor]
+fn proxy_cleanup() {
+    child_cleanup(PROXY.get());
+}
+
+fn child_cleanup(possible_child: Option<&Child>) {
+    // Make sure the server is shut down. This is a little hacky, but
+    // seems to be the only reliable method for a global processs in
+    // rust tests.
+    if let Some(child) = possible_child {
+        let pid = Pid::from_raw(child.id().try_into().unwrap());
+        kill(pid, Signal::SIGINT).unwrap();
+        // Last as we don't know if stdout will work
+        println!("Killed process {}", pid);
+    }
+}
+
+// pub fn stop_child(child: &Child) -> Result<()> {
+//     let pid = Pid::from_raw(child.id().try_into()?);
+//     kill(pid, Signal::SIGINT)?;
+//     Ok(())
+// }
+
+pub struct IntegrationTest {
+    pub proxy: &'static Child,
+}
+
+impl TestContext for IntegrationTest {
+    fn setup() -> Self {
+        let proxy = run_proxy()
+            .expect("Failed to get proxy process");
+        Self {
+            proxy: &proxy,
         }
     }
-    bail!("Failed to find process port");
+
+    fn teardown(self) {
+    }
 }
+
+
+// pub struct MockBackend {
+//     pub handle: JoinHandle<Result<(), std::io::Error>>,
+//     pub msgs: Receiver<String>,
+//     pub quit: Arc<Notify>,
+// }
+
+// impl MockBackend {
+//     pub async fn new() -> Result<MockBackend> {
+//         let ip: IpAddr = "0.0.0.0".parse()?;
+//         let addr = SocketAddr::from((ip, 9090));
+
+//         let quit = Arc::new(Notify::new());
+//         let server_quit = quit.clone();
+
+//         let (tx, rx) = mpsc::channel(8);
+//         let tstate = TelegramState {
+//             recvd_msgs: tx,
+//             quit: quit.clone(),
+//         };
+
+//         let sendmessage_path = format!("/bot{}/sendmessage", BOT_TOKEN);
+//         let setmycommands_path = format!("/bot{}/setMyCommands", BOT_TOKEN);
+//         let getupdates_path = format!("/bot{}/getUpdates", BOT_TOKEN);
+//         let route = Router::new()
+//             .route(sendmessage_path.as_str(), routing::post(sendmessage))
+//             .route(setmycommands_path.as_str(), routing::post(set_my_commands))
+//             .route(getupdates_path.as_str(), routing::get(getupdates))
+//             .with_state(tstate);
+
+//         info!("Starting HTTP server on {}", addr);
+//         let listener = TcpListener::bind(&addr).await?;
+//         let httpd = axum::serve(listener, route)
+//             .with_graceful_shutdown(async move {
+//                 server_quit.notified().await;
+//             })
+//             .into_future();
+
+//         let handle = tokio::spawn(httpd);
+
+//         let server = TelegramServer {
+//             handle,
+//             msgs: rx,
+//             quit,
+//         };
+
+//         Ok(server)
+//     }
+// }
