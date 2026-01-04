a86faca33c93349259b7d29210803b59c8a05eb0
Steve <tarkasteve@gmail.com>
2026-01-04 16:58:56 +1100

    Intial backend mocking test with wiremock.

--- tests/integration/tests.rs
+++ tests/integration/tests.rs
@@ -1,8 +1,12 @@
 
 use reqwest::{Client, redirect};
 use serial_test::serial;
+use wiremock::{
+    Mock, ResponseTemplate,
+    matchers::{method, path},
+};
 
-use crate::util::{INSECURE_PORT, ProxyBuilder, TLS_PORT, mkcert_root};
+use crate::util::{BACKEND_PORT, INSECURE_PORT, ProxyBuilder, TLS_PORT, mkcert_root, mock_server};
 
 // NOTE: We use unwrap rather than result here as it save the run
 // files on failure (in Proxy::drop()).
@@ -36,7 +40,7 @@ async fn test_dns_override() {
         .run().await.unwrap();
 
     let example_com = format!("127.0.0.1:{TLS_PORT}").parse().unwrap();
-    let root_cert = mkcert_root().unwrap();
+    let root_cert = mkcert_root().await.unwrap();
     let ready = Client::builder()
         .resolve("www.example.com", example_com)
         .add_root_certificate(root_cert)
@@ -46,4 +50,33 @@ async fn test_dns_override() {
 
     // No backend, so fails
     assert_eq!(502, ready.status().as_u16());
+}
+
+#[tokio::test]
+#[serial]
+async fn test_mocked_backend() {
+    let backend_server = mock_server(BACKEND_PORT).await.unwrap();
+
+    let _proxy = ProxyBuilder::new().await
+        .with_simple_config("example_com_simple")
+        .run().await.unwrap();
+
+    let example_com = format!("127.0.0.1:{TLS_PORT}").parse().unwrap();
+    let root_cert = mkcert_root().await.unwrap();
+
+    Mock::given(method("GET"))
+        .and(path("/status"))
+        .respond_with(ResponseTemplate::new(200)
+                      .set_body_string("OK"))
+        .mount(&backend_server).await;
+
+    let response = Client::builder()
+        .resolve("www.example.com", example_com)
+        .add_root_certificate(root_cert)
+        .build().unwrap()
+        .get(format!("https://www.example.com:{TLS_PORT}/status"))
+        .send().await.unwrap();
+
+    assert_eq!(200, response.status().as_u16());
+    assert!(response.text().await.unwrap().contains("OK"));
 }
--- tests/integration/util.rs
+++ tests/integration/util.rs
@@ -1,8 +1,7 @@
 #![allow(unused)]
 
-use std::process::Stdio;
+use std::{sync::Arc};
 use std::thread::panicking;
-use std::thread;
 use std::time::Duration;
 
 use anyhow::{Result, anyhow, bail};
@@ -10,13 +9,16 @@ use camino::Utf8PathBuf;
 use nix::{sys::signal::{Signal, kill}, unistd::Pid};
 use reqwest::{Certificate, blocking::Client, redirect};
 use tempfile::{TempDir, tempdir_in};
-use tokio::fs::{File, copy, create_dir_all};
-use tokio::net::TcpStream;
+use tokio::sync::{Notify, mpsc};
+use tokio::{fs::{File, copy, create_dir_all}, sync::mpsc::Receiver, task::JoinHandle};
+use tokio::net::{TcpListener, TcpStream};
 use tokio::process::{Child, Command};
 use tracing_log::log::info;
+use wiremock::MockServer;
 
 pub const INSECURE_PORT: u16 = 8080;
 pub const TLS_PORT: u16 = 8443;
+pub const BACKEND_PORT: u16 = 9090;
 
 pub struct ProxyBuilder {
     pub dir: TempDir,
@@ -68,7 +70,6 @@ impl ProxyBuilder {
         let stdout = File::create(out_file).await?;
         let stderr = File::create(err_file).await?;
 
-
         // Checked above
         let config = self.config.as_ref().unwrap();
         let fname = config.components().last().ok_or(anyhow!("No filename"))?;
@@ -90,7 +91,7 @@ impl ProxyBuilder {
                 info!("Test Proxy Ready");
                 return Ok(child);
             }
-            thread::sleep(Duration::from_millis(100));
+            tokio::time::sleep(Duration::from_millis(100)).await;
         }
         bail!("Failed to start proxy server")
     }
@@ -118,59 +119,18 @@ impl Drop for Proxy {
     }
 }
 
-pub fn mkcert_root() -> Result<Certificate> {
+pub async fn mkcert_root() -> Result<Certificate> {
     let home = std::env::var("HOME")?;
     let certroot = Utf8PathBuf::from(home)
         .join(".local/share/mkcert/rootCA.pem");
-    let pem = std::fs::read(certroot)?;
+    let pem = tokio::fs::read(certroot).await?;
     Ok(reqwest::Certificate::from_pem(&pem)?)
 }
 
-// pub struct MockBackend {
-//     pub handle: JoinHandle<Result<(), std::io::Error>>,
-//     pub msgs: Receiver<String>,
-//     pub quit: Arc<Notify>,
-// }
-
-// impl MockBackend {
-//     pub async fn new() -> Result<MockBackend> {
-//         let ip: IpAddr = "0.0.0.0".parse()?;
-//         let addr = SocketAddr::from((ip, 9090));
-
-//         let quit = Arc::new(Notify::new());
-//         let server_quit = quit.clone();
-
-//         let (tx, rx) = mpsc::channel(8);
-//         let tstate = TelegramState {
-//             recvd_msgs: tx,
-//             quit: quit.clone(),
-//         };
-
-//         let sendmessage_path = format!("/bot{}/sendmessage", BOT_TOKEN);
-//         let setmycommands_path = format!("/bot{}/setMyCommands", BOT_TOKEN);
-//         let getupdates_path = format!("/bot{}/getUpdates", BOT_TOKEN);
-//         let route = Router::new()
-//             .route(sendmessage_path.as_str(), routing::post(sendmessage))
-//             .route(setmycommands_path.as_str(), routing::post(set_my_commands))
-//             .route(getupdates_path.as_str(), routing::get(getupdates))
-//             .with_state(tstate);
-
-//         info!("Starting HTTP server on {}", addr);
-//         let listener = TcpListener::bind(&addr).await?;
-//         let httpd = axum::serve(listener, route)
-//             .with_graceful_shutdown(async move {
-//                 server_quit.notified().await;
-//             })
-//             .into_future();
-
-//         let handle = tokio::spawn(httpd);
-
-//         let server = TelegramServer {
-//             handle,
-//             msgs: rx,
-//             quit,
-//         };
-
-//         Ok(server)
-//     }
-// }
+pub async fn mock_server(port: u16) -> Result<MockServer> {
+    let addr = format!("127.0.0.1:{port}");
+    let listener = TcpListener::bind(addr).await?;
+    let server = MockServer::builder()
+        .listener(listener.into_std()?).start().await;
+    Ok(server)
+}
